2f0bda8b2ab8e2c1529182058536fdbbea6a1f7a
Ignacio Vega <ivegamerayo@gmail.com>
2021-06-02 22:50:19 +0200

    definitivo

--- scriptApache.sh
+++ scriptApache.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 clear
 
+#Colors
+white="\033[1;37m"
+grey="\033[0;37m"
+purple="\033[0;35m"
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+Purple="\033[0;35m"
+Cyan="\033[0;36m"
+Cafe="\033[0;33m"
+Fiuscha="\033[0;35m"
+blue="\033[1;34m"
+nc="\e[0m"
+
+
 #ROOT PRIVILEGIES
 if [[ $EUID -ne 0 ]]; then
     echo -e "$red No estas en root.$nc"
     exit 1
 fi
+
 # Actualizacion del server
+echo -e "                     $blue Actualizacion del server"
+echo -e "$grey"
 apt update -y
 apt upgrade -y
+
 #Configuracion de ip estatica
+echo ""
 echo -e "                     $Cyan Cambio de IP"
 echo ""
 echo -e "$green"
@@ -77,8 +97,13 @@ echo -e " 		[+]$yellow Configurado /etc/fstab con exito $nc[$green✓$nc] $nc[+]
 
 # Introduccion de la informacion basica
 
+echo -e "                    $Cyan Configuracion de FTP y Apache$nc"
+echo ""
+echo -e "$green"
 printf "\nIntroduce la ruta de la carpeta raiz: "
 read carpeta_raiz
+carpeta_raiz = $montar_disco/$carpeta_raiz
+
 mkdir $carpeta_raiz
 
 printf "\nIntroduce el sitio: "
